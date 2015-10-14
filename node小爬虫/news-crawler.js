4b55fa57561ac0e8766dc461f6f59947fdfead9e
manfredHu <279331641@qq.com>
2015-10-14 22:06:28 +0800

    提交小爬虫demo

--- node小爬虫/news-crawler.js
+++ node小爬虫/news-crawler.js
@@ -1,7 +1,7 @@
 var http = require('http')
-var url = 'http://www.imooc.com/learn/348'
+var url = 'http://news.baidu.com/' //百度新闻
 
-// get方法
+// 用get方法发送http请求，在data获取数据
 // @param url http发起请求的url
 // @param callback 回调函数，对返回的response进行处理
 http.get(url, function(res) {
@@ -15,6 +15,6 @@ http.get(url, function(res) {
 		console.log(html)
 	})
 
-}).on('error' ,function() {
+}).on('error', function() {
 	console.log('获取请求数据出错！')
-})
+});
