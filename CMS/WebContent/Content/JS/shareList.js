92285ee711f5be51ca9325179f7a3f6770ab881a
manfredHu <279331641@qq.com>
2015-10-14 22:01:27 +0800

    提交requirejsTest项目

--- CMS/WebContent/Content/JS/shareList.js
+++
@@ -1,9 +0,0 @@
-var now = document.getElementById("pageNow");
-var leftListBottom = document.getElementById("leftListBottom");
-var aList = leftListBottom.getElementsByTagName("a");
-for (var a = 0; a < aList.length; a++) {
-	if (now.innerHTML == aList[a].innerHTML) {
-		aList[a].style.border = "1px solid transparent";
-		aList[a].style.color = "#515151";
-	}
-}
