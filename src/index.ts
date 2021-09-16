36eab92d84b0f64b9fcaa57795fd0ebc70ee2e12
pphu <pphu@tencent.com>
2021-09-16 14:59:10 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// 2. 两数相加
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
